986b433f4fe5be60c0b7f3b6cdd7ec2cff783981
Mike Barkas <mike@barkas.com>
2014-10-16 22:10:14 -0400

    graph

--- client/graph.js
+++ client/graph.js
@@ -2,20 +2,43 @@
 
 Template.graph.helpers({
 
+wx : function () {
+  // Set a default.
+  var city = Session.setDefault('city', 'Charlotte,NC');
+  // Get select list.
+  var city = Session.get('city');
+
+  Meteor.call('getWeather', city, function (error, results) {
+    console.log(results.statusCode, results.content);
+
+    Session.set('wx', JSON.parse(results.content));
+
+  });
+
+  return (Session.get('wx'));
+}
+
 });
 
+// Subtract from Kelvin.  kelvin - 273.15 = f.
+
+Template.basic.events({
+  'change .cities': function (evt, tmpl) {
+    Session.set('city', tmpl.find('.cities').value);
+  }
+});
+
+
+
 Template.graph.rendered = function() {
-
-
-
 
 $('#chart').highcharts({
 
         chart: {
-            type: 'bar'
+            type: 'column'
         },
         title: {
-            text: 'Fruit Consumption'
+            text: 'city title'
         },
         xAxis: {
             categories: ['Apples', 'Bananas', 'Oranges', 'Grapes']
@@ -39,4 +62,4 @@ $('#chart').highcharts({
         credits: { enabled: false}
     });
 
-  }
+}
